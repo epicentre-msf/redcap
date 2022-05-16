7a88a7fff7bced7cac39a84f745b9745e5dc9b81
patrickbarks <patrick.barks@gmail.com>
2022-05-16 16:08:36 +0200

    revert to time_start/end argument names

--- tests/testthat/test-project_logging.R
+++ tests/testthat/test-project_logging.R
@@ -20,20 +20,20 @@ test_that("project_logging works as expected", {
   x5 <- project_logging(conn_test, type = "record")
 
   dt_min <- "2021-03-01 13:30"
-  x6 <- project_logging(conn_test, type = "record", datetime_start = dt_min)
+  x6 <- project_logging(conn_test, type = "record", time_start = dt_min)
   expect_gte(min(x6$timestamp), lubridate::ymd_hm(dt_min))
   expect_equal(max(x6$timestamp), max(x5$timestamp))
 
   dt_max <- "2021-03-19 19:05"
-  x7 <- project_logging(conn_test, type = "record", datetime_end = dt_max)
+  x7 <- project_logging(conn_test, type = "record", time_end = dt_max)
   expect_lte(max(x7$timestamp), lubridate::ymd_hm(dt_max))
   expect_equal(min(x7$timestamp), min(x5$timestamp))
 
-  x8 <- project_logging(conn_test, type = "record", datetime_start = dt_min, datetime_end = dt_max)
+  x8 <- project_logging(conn_test, type = "record", time_start = dt_min, time_end = dt_max)
   expect_gte(min(x8$timestamp), lubridate::ymd_hm(dt_min))
   expect_lte(max(x8$timestamp), lubridate::ymd_hm(dt_max))
 
-  x9 <- project_logging(conn_test, record = "0002", datetime_to_posix = FALSE)
+  x9 <- project_logging(conn_test, record = "0002", timestamp_to_posix = FALSE)
   expect_type(x9$timestamp, "character")
 })
 
